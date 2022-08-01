6f15947b6a949f318a7b287c7119fa1084b84eee
Pimenta <mateuspimartins@hotmail.com>
2022-08-01 15:10:14 -0300

    feat: add random page test

--- front-end/cypress/e2e/top_random_pages.cy.js
+++ front-end/cypress/e2e/top_random_pages.cy.js
@@ -2,7 +2,7 @@
 
 import { faker } from "@faker-js/faker";
 
-before(() => {
+beforeEach(() => {
   cy.resetPosts();
 });
 
@@ -33,18 +33,12 @@ describe("top and random pages suite", () => {
     }
     cy.visit("http://localhost:3000");
     for (let i = 0; i < 10; i++) {
-      cy.intercept("POST", "/recommendations/2/upvote").as("upvotePost2");
+      cy.intercept("POST", "/recommendations/2/upvote").as("upvotePost");
       cy.contains(musics[1].name)
         .parent()
         .find('[data-identifier="upvote"]')
         .click();
-      cy.wait("@upvotePost2");
-      cy.intercept("POST", "/recommendations/1/upvote").as("upvotePost1");
-      cy.contains(musics[0].name)
-        .parent()
-        .find('[data-identifier="upvote"]')
-        .click();
-      cy.wait("@upvotePost1");
+      cy.wait("@upvotePost");
       if (i % 3 === 0) {
         cy.intercept("POST", "/recommendations/3/downvote").as("downvotePost");
         cy.contains(musics[2].name)
@@ -54,12 +48,6 @@ describe("top and random pages suite", () => {
         cy.wait("@downvotePost");
       }
     }
-    cy.intercept("POST", "/recommendations/1/downvote").as("downvotePost1");
-    cy.contains(musics[0].name)
-      .parent()
-      .find('[data-identifier="downvote"]')
-      .click();
-    cy.wait("@downvotePost1");
 
     cy.contains("Top").click();
     cy.url().should("equal", "http://localhost:3000/top");
@@ -73,7 +61,89 @@ describe("top and random pages suite", () => {
       .should("have.text", "-4");
   });
 
-  it("")
+  it("with two post, should update ranking after change the votes", () => {
+    const musics = [];
+    for (let i = 0; i < 2; i++) {
+      const musicData = {
+        name: faker.name.findName(),
+        youtubeLink: "https://youtu.be/ALZHF5UqnU4",
+      };
+      musics.push(musicData);
+      cy.createPost(musicData);
+    }
+    cy.visit("http://localhost:3000");
+    for (let i = 0; i < 3; i++) {
+      cy.intercept("POST", "/recommendations/2/upvote").as("upvotePost1");
+      cy.contains(musics[1].name)
+        .parent()
+        .find('[data-identifier="upvote"]')
+        .click();
+      cy.wait("@upvotePost1");
+      cy.intercept("POST", "/recommendations/1/upvote").as("upvotePost2");
+      cy.contains(musics[0].name)
+        .parent()
+        .find('[data-identifier="upvote"]')
+        .click();
+      cy.wait("@upvotePost2");
+    }
+    cy.intercept("POST", "/recommendations/2/upvote").as("upvotePost1");
+    cy.contains(musics[1].name)
+      .parent()
+      .find('[data-identifier="upvote"]')
+      .click();
+    cy.wait("@upvotePost1");
 
-  it("with three posts, get one post at random page", () => {});
+    cy.contains("Top").click();
+    cy.url().should("equal", "http://localhost:3000/top");
+    cy.get("article")
+      .should("have.length", 2)
+      .first()
+      .should("contain", musics[1].name)
+      .find('[data-identifier="vote-menu"]')
+      .should("have.text", "4");
+    cy.get("article")
+      .last()
+      .find('[data-identifier="vote-menu"]')
+      .should("have.text", "3");
+
+    cy.intercept("POST", "/recommendations/1/upvote").as("upvotePost2");
+    cy.get("article").last().find('[data-identifier="upvote"]').click();
+    cy.wait("@upvotePost2");
+
+    cy.intercept("POST", "/recommendations/1/upvote").as("upvotePost2");
+    cy.get("article").last().find('[data-identifier="upvote"]').click();
+    cy.wait("@upvotePost2");
+
+    cy.intercept("GET", "/recommendations/top/10").as("reload");
+    cy.reload();
+    cy.wait("@reload");
+
+    cy.get("article")
+      .should("have.length", 2)
+      .first()
+      .should("contain", musics[0].name)
+      .find('[data-identifier="vote-menu"]')
+      .should("have.text", "5");
+  });
+
+  it("with three posts, get one post at random page", () => {
+    const musics = [];
+    for (let i = 0; i < 3; i++) {
+      const musicData = {
+        name: faker.name.findName(),
+        youtubeLink: "https://youtu.be/ALZHF5UqnU4",
+      };
+      musics.push(musicData);
+      cy.createPost(musicData);
+    }
+    cy.visit("http://localhost:3000");
+
+    cy.intercept("GET", "http://localhost:5000/recommendations/random").as(
+      "random"
+    );
+    cy.contains("Random").click();
+    cy.wait("@random");
+
+    cy.get("article").should("have.length", 1);
+  });
 });
